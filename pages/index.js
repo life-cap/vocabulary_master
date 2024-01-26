8278adbc928d5c4758ad4a76e8b2796e26c97bbc
life-cap <rnaudah923@gmail.com>
2024-01-26 09:50:29 +0900

    변경

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
             };
           }
         });
-        setNews(newsData.slice(0, 10));
+        setNews(newsData.slice(0, 20));
         const quoteResponse = await axios.get("https://api.quotable.io/quotes/random?tags=famous-quotes");
         const quoteData = quoteResponse.data[0];
         setQuote([quoteData.author, quoteData.content]);
